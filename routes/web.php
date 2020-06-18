311bff8eb369499c2bb668dd562b022baf50e30b
y.maeda <yuyuyu.mae606@gmail.com>
2020-06-18 17:28:51 +0900

    写真詳細機能

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 // 写真ダウンロード
 Route::get('/photos/{photo}/download', 'PhotoController@download');
-
+Route::get('/photos/{id}', 'PhotoController@show')->name('photo.show');
 Route::get('/{any?}', fn() => view('index'))->where('any', '.+');
+
 
