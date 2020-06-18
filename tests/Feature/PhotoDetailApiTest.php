1b2c93ee33428e2cfc61cfd85b095f5c97fed448
y.maeda <yuyuyu.mae606@gmail.com>
2020-06-18 17:28:09 +0900

    写真詳細テスト

---
+++ tests/Feature/PhotoDetailApiTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Tests\Feature;
+
+
+use App\Photo;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class PhotoDetailApiTest extends TestCase
+{
+  use RefreshDatabase;
+
+  /**
+   * @test
+   */
+  public function should_正しい構造のjsonデータを返却する()
+  {
+    //testデータを作成
+    factory(Photo::class)->create();
+    //登録したデータを取得
+    $photo = Photo::first();
+    //photoの詳細情報取得アクションにアクセスし、レスポンスデータを取得
+    $response = $this->json('get', route('photo.show',[
+      'id' => $photo->id,
+    ]));
+    
+    $response->assertStatus(200)
+    //assertJsonFragment()でJsonのフォーマットを確かめる
+            ->assertJsonFragment([
+              'id' => $photo->id,
+              'url' => $photo->url,
+              'owner' => [
+                'name' => $photo->owner->name,
+              ],
+            ]);
+  }
+
+}
