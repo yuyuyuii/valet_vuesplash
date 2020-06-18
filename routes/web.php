af906d4295774a88df4622347a968d32bf48a8af
y.maeda <yuyuyu.mae606@gmail.com>
2020-06-18 18:13:28 +0900

    ルーティング修正

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 // 写真ダウンロード
 Route::get('/photos/{photo}/download', 'PhotoController@download');
-Route::get('/photos/{id}', 'PhotoController@show')->name('photo.show');
 Route::get('/{any?}', fn() => view('index'))->where('any', '.+');
 
 
