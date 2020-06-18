1df0c01c6eafe0f91ed6025f79899e34a1416bf2
y.maeda <yuyuyu.mae606@gmail.com>
2020-06-18 18:30:05 +0900

    修正

--- resources/js/pages/photoDetail.vue
+++ resources/js/pages/photoDetail.vue
@@ -1,5 +1,67 @@
 <template>
-  <div>
-    <h2>Photo Detail</h2>
+  <div
+    v-if="photo"
+    class="photo-detail"
+    :class="{ 'photo-detail--column': fullWidth }"
+  >
+    <figure
+      class="photo-detail__pane photo-detail__image"
+      @click="fullWidth = ! fullWidth"
+    >
+      <img :src="photo.url" alt="">
+      <figcaption>Posted by {{ photo.owner.name }}</figcaption>
+    </figure>
+    <div class="photo-detail__pane">
+      <button class="button button--like" title="Like photo">
+        <i class="icon ion-md-heart"></i>12
+      </button>
+      <a
+        :href="`/photos/${photo.id}/download`"
+        class="button"
+        title="Download photo"
+      >
+        <i class="icon ion-md-arrow-round-down"></i>Download
+      </a>
+      <h2 class="photo-detail__title">
+        <i class="icon ion-md-chatboxes"></i>Comments
+      </h2>
+    </div>
   </div>
 </template>
+
+<script>
+import { OK } from '../util'
+export default {
+  props: {
+    id: {
+      type: String,
+      required: true
+    }
+  },
+  data () {
+    return {
+      photo: null,
+      fullWidth: false
+    }
+  },
+  methods: {
+    async fetchPhoto () {
+      const response = await axios.get(`/api/photos/${this.id}`)
+      if (response.status !== OK) {
+        this.$store.commit('error/setCode', response.status)
+        return false
+      }
+    console.log(response)
+      this.photo = response.data
+    }
+  },
+  watch: {
+    $route: {
+      async handler () {
+        await this.fetchPhoto()
+      },
+      immediate: true
+    }
+  }
+}
+</script>
